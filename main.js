497994c64d1e134fdfc45957eae93444514d92e7
FLAVIE <flavie@MacBook-Pro-de-FLAVIE.local>
2021-04-06 00:06:53 +0200

    JS_Exercice_P2_3

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+
+// la fonction ShowAlert affiche dans une modale  le caractére à chaque saisi 
+// avec l'evenement onkeyup (appeler dans le html)
+function showAlert() {
+      var typeText = document.getElementById('focus').value;
+            alert(typeText);
+      }
